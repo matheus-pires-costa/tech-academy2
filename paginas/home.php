d1b02faa5cec0dfd96246a496480d5245c8f0355
matheus-pires-costa <matheus.pires2099@gmail.com>
2025-11-04 18:58:25 -0300

    terminado a primeira fase do site, indo para a segunda, foi feitoo titulo 'Quem Sou Eu' e colocado abaixo o espaço para o video, ainda esta sujeito a modificaçoes para fins de melhor experiencia e responsividade, video ainda nao adicionado

--- paginas/home.php
+++ paginas/home.php
@@ -1,20 +1,33 @@
 <div class="container">
     <div class="headline">
-        <h1 class="text-center">Pele <span style="color:rgb(255, 204, 0) ">Radiante</span> e Corpo <span style="color:rgb(255, 204, 0) ">Renovado</span>. Comece Sua Rotina de Cuidado, <span style="color:rgb(255, 204, 0) ">Agende Já !</span></h1>
+        <h1 class="text-center">Pele <span style="color:rgb(255, 204, 0) ">Radiante</span> e Corpo <span
+                style="color:rgb(255, 204, 0) ">Renovado</span>. Comece Sua Rotina de Cuidado, <span
+                style="color:rgb(255, 204, 0) ">Agende Já !</span></h1>
     </div>
 
     <div class="apresentacao">
         <img src="imagens/imagem1.jpeg" alt="Apresentaçao de Serviços" title="Apresentaçao de Serviços">
     </div>
-    
+
     <div class="text-center my-4">
 
-      <a href="https://bit.ly/4biyKWU" class="btn btn-success btn-lg fw-bold" role="button">
-        
-        Agendar minha Avaliação! 
-        
-        <i class="bi bi-whatsapp ms-2"></i>
-      </a>
+        <a href="https://bit.ly/4biyKWU" class="btn btn-success btn-lg fw-bold" role="button">
+
+            Agendar minha Avaliação!
+
+            <i class="bi bi-whatsapp ms-2"></i>
+        </a>
+
+    </div>
+    <div class="titulo" class="text-center">
+        <h2 class="titulo-letra">
+            Quem sou eu?
+        </h2>
+    </div>
+    <div class="video">
+        <video src="caminho/para/seu/video.mp4" controls class="w-100">
+            Seu navegador não suporta a tag de vídeo.
+        </video>
 
     </div>
 
